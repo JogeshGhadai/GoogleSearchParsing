603907a33852d386776804962ab016abc1a2e42b
JogeshGhadai <jogeshg@mindfiresolutions.com>
2020-11-24 11:17:01 +0530

    Restructured the code 2

--- Assignment/GoogleCustomSearchParser.py
+++ Assignment/GoogleCustomSearchParser.py
@@ -5,24 +5,47 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_result_dict(all_conf):
-    res_dict = {}
-    text = input("Enter the keyword to search: ")
-    print("Fetching Results from Google...")
-    res_dict["keyword"] = text
+def get_all_config(conf_file_path):
+    all_conf = None
+    
+    try:
+        with open(conf_file_path) as f:
+            all_conf = yaml.load(f, Loader=yaml.FullLoader)
+    
+    except:
+        print("Error in opening the config file.")
+    
+    finally:
+        return all_conf
+
+
+def get_web_agent(all_conf):
+    selected_agent = None
+    
+    try:
+        all_agents_list = all_conf["agents_list"]
+        selected_agent = all_agents_list[random.randrange(len(all_agents_list))]
+    
+    except:
+        print("Error in fetching the agents list from config file.")
+    
+    finally:
+        return selected_agent
+
+
+def get_result_dict(all_conf, text):
     url = all_conf["search_url"] + text
-
-    all_agents_list = all_conf["agents_list"]
-    selected_agent = all_agents_list[random.randrange(len(all_agents_list))]
+    selected_agent = get_web_agent(all_conf)
     headers = {'user-agent': selected_agent}
-    r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.text, 'html.parser')
-
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
     all_top = soup.find_all('g-scrolling-carousel')
     top_results = all_top[0].find_all('div',class_='JJZKK') if len(all_top)>0 else ""
     results = soup.find_all('div',class_='rc')
 
     counter = 0
+    res_dict = {}
+    res_dict["keyword"] = text
     res_dict["results"] = {}
     res_dict["results"]["top_results"] = {}
     res_dict["results"]["organic_results"] = {}
@@ -39,6 +62,7 @@ def get_result_dict(all_conf):
         res_dict["results"]["top_results"][counter]= indi_dict
 
     counter = 0
+    
     for result in results:
         counter += 1
         indi_dict = {}
@@ -51,22 +75,17 @@ def get_result_dict(all_conf):
     return res_dict
 
 
-def get_all_config(conf_file_path):
-    all_conf = None
-    try:
-        with open(conf_file_path) as f:
-            all_conf = yaml.load(f, Loader=yaml.FullLoader)
-    except:
-        all_conf = None
-        print("Error in opening the config file.")
-    finally:
-        return all_conf
+def write_results_to_json(all_conf, text):
+    
+    with open(f"{all_conf["out_file_path"]}results_{text.replace(" ", "_")}.json","w") as fl:
+        json.dump(res_dict,fl,indent=2)
+    
+    print("Data fetched and written to JSON Successfully.")
 
 
 if __name__ == "__main__":
+    text = input("Enter the keyword to search: ")
+    print("Fetching Results from Google...")
     all_conf = get_all_config("./config.yaml")
-    res_dict = get_result_dict(all_conf)
-    with open(f"{all_conf["out_file_path"]}results_{text.replace(" ", "_")}.json","w") as fl:
-        json.dump(res_dict,fl,indent=2)
+    res_dict = get_result_dict(all_conf, text)
 
-    print("Data fetched and written to JSON Successfully.")
